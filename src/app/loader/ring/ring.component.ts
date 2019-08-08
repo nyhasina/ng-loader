1a25058519a7c1841ed2d15a459c0c1f27f25eda
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-08 17:26:52 +0300

    [LOADER] Create ring loader component #master

---
+++ src/app/loader/ring/ring.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ring',
+  templateUrl: './ring.component.html',
+  styleUrls: ['./ring.component.scss']
+})
+export class RingComponent {
+}
