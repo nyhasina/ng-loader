567000c66d004181e33575f0744eba2c4de7d884
Ny Hasinavalona Randriantsarafara <randriantsarafaranyhasinavalon@gmail.com>
2019-08-08 17:27:18 +0300

    [APP] Add component context #master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Label {
+  label: string;
+  value: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-loader';
+
+export class AppComponent implements OnInit {
+  data: Label[] = [];
+  show = false;
+
+  ngOnInit(): void {
+    this.data = new Array(20).fill({}).map((v, i) => ({ label: `Element ${i + 1}`, value: i + 1}));
+  }
 }
